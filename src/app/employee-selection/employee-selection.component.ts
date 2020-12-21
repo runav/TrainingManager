3961867210e0e01b769132e14909ebef87290b37
runav <75965709+runav@users.noreply.github.com>
2020-12-21 15:47:26 -0800

    First Commit

---
+++ src/app/employee-selection/employee-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-selection',
+  templateUrl: './employee-selection.component.html',
+  styleUrls: ['./employee-selection.component.css']
+})
+export class EmployeeSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
